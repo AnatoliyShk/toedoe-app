48e7f432d7f39eb10b7dbd627b50a2b99286fced
Anatoliy Shekera <tolikshekera@gmail.com>
2026-02-22 03:57:13 +0200

    Refactioring

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,23 +1,46 @@
 import { completeTask } from '@/http/task-api';
 import { defineStore } from 'pinia';
 import { allTasks } from '../http/task-api';
-
-export const useTaskStore = defineStore('taskStore', {
+import { ref, reactive, computed } from 'vue';
+const tmp = {
     state: () => ({
         tasks: [],
     }),
     getters: {
-        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
-        uncompletedTasks: (state) => state.tasks.filter(task => !task.is_completed),
     },
     actions: {
-        async fetchAllTasks() {
-            try {
-                const { data } = await allTasks();
-                this.tasks = data.data;
-            } catch (error) {
-                console.error('Error fetching tasks:', error);
-            }
+    }
+}
+export const useTaskStore = defineStore('taskStore', () => {
+    const tasks = ref([]);
+    const task = reactive({
+        id: null,
+        name: null,
+        is_completed: false,
+
+    })
+
+    const uncompletedTasks = computed(() =>
+        tasks.value.filter(task => !task.is_completed)
+    );
+
+    const completedTasks = computed(() =>
+        tasks.value.filter(task => task.is_completed)
+    );
+
+    const fetchAllTasks = async () => {
+        try {
+            const { data } = await allTasks();
+            tasks.value = data.data;
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
         }
+    }
+
+    return {
+        tasks,
+        completedTasks,
+        uncompletedTasks,
+        fetchAllTasks
     }
 }); 
