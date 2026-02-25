21f52fae30e8128e34a2d895aa6727e5198b9368
Anatoliy Shekera <tolikshekera@gmail.com>
2026-02-25 02:19:02 +0200

    Navbar and summary page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,13 @@
 import TaskPage from '../pages/TaskPage.vue';
 import LoginPage from '../pages/LoginPage.vue';
 import RegisterPage from '../pages/RegisterPage.vue';
+import SummaryPage from '../pages/SummaryPage.vue';
 
 const routes = [
     { path: '/tasks', component: TaskPage, name: 'tasks' },
     { path: '/login', component: LoginPage, name: 'login' },
     { path: '/register', component: RegisterPage, name: 'register' },
+    { path: '/summary', component: SummaryPage, name: 'summary' },
 ];
 
 export default routes;
