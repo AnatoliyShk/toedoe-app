fba889f20ac86392e56d34fd98fc6a5c2631019b
Anatoliy Shekera <tolikshekera@gmail.com>
2026-02-25 03:23:56 +0200

    Navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import routes from './routes';
 
-export default createRouter({
+const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+router.beforeEach((to, from) => {
+    return { name: "login"};
+});
+
+export default router;
