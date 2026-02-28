243d5a28eae6a10b8264a93ee11e1e0c2fa4b5e9
Anatoliy Shekera <tolikshekera@gmail.com>
2026-02-28 22:25:49 +0200

    Auth api

---
+++ src/http/auth-api.js
@@ -0,0 +1,17 @@
+import api from "./api";
+
+export const csrfCookie = () => {
+  return api.get('/sanctum/csrf-cookie');
+}
+
+export const login = (credentials) => {
+  return api.post('/login', credentials);
+}
+
+export const register = (user) => {
+  return api.post('/register', user);
+}
+
+export const logout = () => {
+  return api.post('/logout');
+}
